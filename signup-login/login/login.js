cb9031b25b4c724180a959ef202d337864c85250
SBP-Learner <sbpd206@gmail.com>
2025-12-19 22:53:41 +0530

    Sign-Up Login UI update

---
+++ signup-login/login/login.js
@@ -0,0 +1,18 @@
+document.querySelectorAll(".toggle-password").forEach(toggle => {
+    toggle.addEventListener("click", function () {
+        const passwordField = this.closest(".password-field");
+        const input = passwordField.querySelector("input");
+        const eyeOpen = passwordField.querySelector(".eye-open");
+        const eyeClosed = passwordField.querySelector(".eye-closed");
+
+        if (input.type === "password") {
+            input.type = "text";
+            eyeOpen.style.display = "block";
+            eyeClosed.style.display = "none";
+        } else {
+            input.type = "password";
+            eyeOpen.style.display = "none";
+            eyeClosed.style.display = "block";
+        }
+    });
+});
