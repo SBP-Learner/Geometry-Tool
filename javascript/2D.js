9227639e02e6a91c9b29121ad9502ada7a33e335
SBP-Learner <sbpd206@gmail.com>
2025-12-27 19:56:26 +0530

    2D-3D page shape slider and text changes

---
+++ javascript/2D.js
@@ -0,0 +1,21 @@
+
+let index = 0;
+const track = document.getElementById("slide");
+const total = track.children.length;
+const width = 320;
+
+function update() {
+  track.style.transform = `translateX(-${index * width}px)`;
+}
+
+function next() {
+  index = (index + 1) % total;
+  update();
+}
+
+function prev() {
+  index = (index - 1 + total) % total;
+  update();
+}
+
+setInterval(next, 3000);
